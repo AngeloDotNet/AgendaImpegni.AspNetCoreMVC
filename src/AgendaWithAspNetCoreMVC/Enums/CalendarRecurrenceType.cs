93ab854d273f5ee23681c23e10e5cf10c419fef2
Angelo Pirola <angelo9871@gmail.com>
2026-08-16 16:06:20 +0200

    Add CalendarRecurrenceType and CalendarReminderType enums
    Added two new enums in AgendaWithAspNetCoreMVC.Enums:
    - CalendarRecurrenceType: None, Daily, Weekly, Monthly, Yearly
    - CalendarReminderType: None, AtTime, FiveMinutesBefore, FifteenMinutesBefore, ThirtyMinutesBefore, OneHourBefore, OneDayBefore

---
+++ src/AgendaWithAspNetCoreMVC/Enums/CalendarRecurrenceType.cs
@@ -0,0 +1,10 @@
+﻿namespace AgendaWithAspNetCoreMVC.Enums;
+
+public enum CalendarRecurrenceType
+{
+	None = 0,
+	Daily = 1,
+	Weekly = 2,
+	Monthly = 3,
+	Yearly = 4
+}
---
+++ src/AgendaWithAspNetCoreMVC/Enums/CalendarReminderType.cs
@@ -0,0 +1,12 @@
+﻿namespace AgendaWithAspNetCoreMVC.Enums;
+
+public enum CalendarReminderType
+{
+	None = 0,
+	AtTime = 1,
+	FiveMinutesBefore = 2,
+	FifteenMinutesBefore = 3,
+	ThirtyMinutesBefore = 4,
+	OneHourBefore = 5,
+	OneDayBefore = 6
+}
