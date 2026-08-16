50ef495c620762ab9c85829280ca6ddd853fc2a9
Angelo Pirola <angelo9871@gmail.com>
2026-08-16 16:07:24 +0200

    Disable AgendaPageViewModel, add AgendaSidebarViewModel
    AgendaPageViewModel was commented out to disable it. Introduced AgendaSidebarViewModel with properties for view state, date, title, query, category ID, categories, and day events, utilizing CalendarCategory and CalendarEvent models.

---
+++ src/AgendaWithAspNetCoreMVC/ViewModels/AgendaPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AgendaWithAspNetCoreMVC.ViewModels;
+
+//public class AgendaPageViewModel
+//{
+//	public string InitialView { get; set; } = "dayGridMonth";
+//	public DateTime InitialDate { get; set; } = DateTime.Today;
+//	public string Title => "Agenda personale";
+
+//	public List<CalendarCategory> Categories { get; set; } = new();
+//	public List<CalendarEvent> TodayEvents { get; set; } = new();
+//	public string? Query { get; set; }
+//	public Guid? CategoryId { get; set; }
+//}
---
+++ src/AgendaWithAspNetCoreMVC/ViewModels/AgendaSidebarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AgendaWithAspNetCoreMVC.Models;
+
+namespace AgendaWithAspNetCoreMVC.ViewModels;
+
+public class AgendaSidebarViewModel
+{
+	public string InitialView { get; set; } = "dayGridMonth";
+	public DateTime InitialDate { get; set; } = DateTime.Today;
+	public string Title => "Agenda personale";
+
+	public string? Query { get; set; }
+	public Guid? CategoryId { get; set; }
+	public List<CalendarCategory> Categories { get; set; } = [];
+	public List<CalendarEvent> DayEvents { get; set; } = [];
+}
